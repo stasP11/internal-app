4be3146c8d64bde878aef7716cc4ed59bb639cd8
stasP11 <stanislav_polivoda@epam.com>
2024-06-27 15:13:50 +0200

    fast fix

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "../pages/Login";
 import ProtectedPages from "./ProtectedPages";
 import MainPageRouts from "./MainPageRouts";
 
