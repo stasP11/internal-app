80955c46a5fee8e231fba71d27ef48bc0a4dbf36
stasP11 <stanislav_polivoda@epam.com>
2024-07-01 11:05:04 +0200

    fix

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,13 +1,22 @@
 import './ErrorPage.scss';
-
+import { useMsal } from "@azure/msal-react";
 
 function Error({errorText}: any){
-      console.log(errorText, 'errorText')
+    const { accounts, inProgress, instance } = useMsal();
+
+    const handleLogoutRedirect = () => {
+      console.log('logout')
+      instance.logoutRedirect().catch((error) => console.log(error));
+    };
+
     return (
     <div className="error-wrapper">
     <div className="error-content">
         <h1 className="error-content__header">Error</h1>
         <p className="error-content__text">{errorText}</p>
+        <button className="error-content__button" onClick={handleLogoutRedirect}>
+        <span className='error-content__button-text'
+        >Sign Out</span></button>
     </div>
     </div>)
 }
